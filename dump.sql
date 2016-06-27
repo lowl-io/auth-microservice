c30fb62d47fba3cb4261498d3631e088b483b82f
s0lus <mashenkoalex@gmail.com>
2016-06-27 11:10:43 +0900

    current_status <-> status

--- dump.sql
+++ dump.sql
@@ -1,11 +1,11 @@
-CREATE TYPE status AS ENUM ('active', 'deleted', 'blocked');
+CREATE TYPE current_status AS ENUM ('active', 'deleted', 'blocked');
 
 CREATE TABLE Users
 (
-  id             SERIAL      NOT NULL PRIMARY KEY,
-  name           VARCHAR(60) NOT NULL,
-  password       VARCHAR(64) NOT NULL,
-  email          VARCHAR(255),
-  current_status status
+  id       SERIAL      NOT NULL PRIMARY KEY,
+  name     VARCHAR(60) NOT NULL,
+  password VARCHAR(64) NOT NULL,
+  email    VARCHAR(255),
+  status   current_status
 );
 
