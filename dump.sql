2dccfbab4c7d2d166530a676e5458c28379a27b7
s0lus <mashenkoalex@gmail.com>
2016-06-29 21:06:17 +0900

    Add records for testing queries

--- dump.sql
+++ dump.sql
@@ -4,8 +4,12 @@ CREATE TABLE Users
 (
   id       SERIAL      NOT NULL PRIMARY KEY,
   name     VARCHAR(60) NOT NULL,
-  password VARCHAR(64) NOT NULL,
+  password VARCHAR(60) NOT NULL,
   email    VARCHAR(255),
   status   current_status
 );
 
+INSERT INTO Users VALUES
+  ('0', 'Alexander', 'password', 'example@gmail.com', 'active'),
+  ('1', 'Dmitry', 'password1', 'example1@mail.ru', 'blocked'),
+  ('2', 'Daniel', 'password2', 'example2@gmail.com', 'deleted');
