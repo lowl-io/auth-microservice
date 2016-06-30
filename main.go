154dba53736318451e0f3a451f2e94306a36fa08
s0lus <mashenkoalex@gmail.com>
2016-06-30 11:48:30 +0900

    Add checkPassword method,add connection to database and simple query

--- main.go
+++ main.go
@@ -2,14 +2,31 @@ package main
 
 import (
 	"github.com/go-martini/martini"
-	_ "github.com/jinzhu/gorm"
+	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	_ "github.com/lib/pq"
 	"net/http"
 )
 
+func (user User) checkPassword(possiblePassword string) bool {
+	if user.password == possiblePassword {
+		return true
+	}
+
+	return false
+}
+
 func main() {
 	m := martini.Classic()
+
+	db, err := gorm.Open("postgres", "user=postgres dbname=postgresdb password=superuser1.")
+	if err != nil {
+		panic("Failed to connect database")
+	}
+
+	var user User
+
+	db.Where("name = ?", "Alexander").Find(&user)
 
 	m.Post("/token", func(request *http.Request) (int, string) {
 		err := request.ParseForm()
@@ -18,13 +35,14 @@ func main() {
 		}
 
 		username := request.Form.Get("username")
-		if username == "" {
-			return http.StatusBadRequest, "Parameter 'username' is not valid"
+		password := request.Form.Get("password")
+
+		if username == "" || password == "" {
+			return http.StatusBadRequest, "Parameter 'username' or 'password' is not valid"
 		}
 
-		password := request.Form.Get("password")
-		if password == "" {
-			return http.StatusBadRequest, "Parameter 'password' is not valid"
+		if user.name == username {
+			user.checkPassword(password)
 		}
 
 		return http.StatusCreated, "Created"
