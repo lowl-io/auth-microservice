e6d6a637dba10ea5693364c5015507b7617f26c8
s0lus <mashenkoalex@gmail.com>
2016-07-07 11:30:31 +0900

    Remove json annotation

--- models.go
+++ models.go
@@ -1,7 +1,7 @@
 package main
 
 type User struct {
-	ID       int       `json:"id";gorm:"not null;type:serial;primary_key"`
+	ID       int       `gorm:"not null;type:serial;primary_key"`
 	Name     string    `gorm:"not null;type:varchar(60);unique_index"`
 	Email    string    `gorm:"type:varchar(255);unique_index"`
 	Password string    `gorm:"not null;type:varchar(60)"`
