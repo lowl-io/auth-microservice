c3374213cfba88224eb3467857777b966e48465e
s0lus <mashenkoalex@gmail.com>
2016-07-14 20:08:35 +0900

    Update dump

--- dump.sql
+++ dump.sql
@@ -3,13 +3,9 @@ CREATE TYPE current_status AS ENUM ('active', 'deleted', 'blocked');
 CREATE TABLE Users
 (
   id       SERIAL       NOT NULL PRIMARY KEY,
-  name     VARCHAR(60)  NOT NULL UNIQUE,
-  password VARCHAR(60)  NOT NULL,
-  email    VARCHAR(255) UNIQUE,
+  email    VARCHAR(255) NOT NULL UNIQUE,
+  username VARCHAR(60)  NOT NULL UNIQUE,
+  password VARCHAR(255)  NOT NULL,
+  name     VARCHAR(60)  NOT NULL,
   status   current_status
 );
-
-INSERT INTO Users VALUES
-  ('0', 'Alexander', 'password', 'example@gmail.com', 'active'),
-  ('1', 'Dmitry', 'password1', 'example1@mail.ru', 'blocked'),
-  ('2', 'Daniel', 'password2', 'example2@gmail.com', 'deleted');
