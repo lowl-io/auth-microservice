7a739a5ee2b2e4d87e7582757cc67d151b2900a8
s0lus <mashenkoalex@gmail.com>
2016-07-14 20:04:41 +0900

    Update struct, add username field

--- models.go
+++ models.go
@@ -3,15 +3,16 @@ package main
 type User struct {
 	ID       int       `gorm:"not null;type:serial;primary_key"`
 	Name     string    `gorm:"not null;type:varchar(60);unique_index"`
+	Username string	   `gorm:"not null;type:varchar(60);unique_index"`
 	Email    string    `gorm:"type:varchar(255);unique_index"`
-	Password string    `gorm:"not null;type:varchar(60)"`
+	Password string    `gorm:"not null;type:varchar(256)"`
 	Status   string
 }
 
 type Config struct {
 	DataBase struct {
-		Dialect 		  string
-		ConnectionData    string
+		Dialect        string
+		ConnectionData string
 	}
 	JWT struct {
 		Key string
