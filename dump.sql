120545a7a69eaa46614984d762f1b6053fc176db
Alexander Mashchenko <mashenkoalex@gmail.com>
2016-07-16 13:10:06 +0900

    Update dump.sql

--- dump.sql
+++ dump.sql
@@ -3,9 +3,9 @@ CREATE TYPE status AS ENUM ('active', 'deleted', 'blocked');
 CREATE TABLE Users
 (
   id       SERIAL       NOT NULL PRIMARY KEY,
-  name     VARCHAR(60)  NOT NULL UNIQUE,
-  password VARCHAR(60)  NOT NULL,,
-  email    VARCHAR(255) UNIQUE,
+  email    VARCHAR(255) NOT NULL UNIQUE,
+  username VARCHAR(60)  NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
   name     VARCHAR(60)  NOT NULL,
   status   current_status
 );
