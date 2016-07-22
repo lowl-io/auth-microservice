2ef96f9507a28c6dcfd1614ba086aea91aa1d96d
s0lus <mashenkoalex@gmail.com>
2016-07-22 14:39:58 +0300

    Delete equivalent code, added check login by uid

---
+++ main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/go-martini/martini"
+	"github.com/dgrijalva/jwt-go"
+	"golang.org/x/crypto/sha3"
+	"github.com/jinzhu/gorm"
+	"encoding/json"
+	"encoding/hex"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"fmt"
+)
+
+func (user User) checkPassword(possiblePassword string) bool {
+	hash := sha3.New256()
+	hash.Write([]byte(possiblePassword))
+	possiblePassword = hex.EncodeToString(hash.Sum(nil))
+
+	return user.Password == possiblePassword
+}
+
+func jsonResponse(response interface{}, w http.ResponseWriter) {
+
+	json, error := json.Marshal(response)
+	if error != nil {
+		fmt.Errorf("Error creating json")
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(json)
+}
+
+func tokenHandler(response http.ResponseWriter, request *http.Request, config Config, db *gorm.DB) (int, string) {
+	error := request.ParseForm()
+	if error != nil {
+		return http.StatusBadRequest, "Incorrect POST request"
+	}
+
+	login := request.Form.Get("username")
+	password := request.Form.Get("password")
+
+	if login == "" || password == "" {
+		return http.StatusBadRequest, "Parameter 'username' or 'password' is not valid"
+	}
+
+	var user User
+
+	if strings.Contains(login, "@") {
+		db.Where("email = ?", login).Find(&user)
+	} else {
+		db.Where("username = ?", login).Find(&user)
+	}
+
+	if user.ID == 0 {
+		return http.StatusNotFound, "User with current 'username' not found by login"
+	}
+	
+	if !user.checkPassword(password) {
+		return http.StatusForbidden, "Parameter 'username' or 'password' is not valid"
+	}
+
+	signer := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"uid" : user.ID,
+	})
+
+	tokenString, error := signer.SignedString([]byte(config.JWT.Key))
+	if error != nil {
+		return http.StatusBadRequest, "Error when signing token"
+	}
+
+	token := Token{tokenString}
+	jsonResponse(token, response)
+
+	return http.StatusCreated, ""
+}
+
+func main() {
+	var config Config
+
+	jsonStream, error := ioutil.ReadFile("config.json")
+	if error != nil {
+		fmt.Errorf("Error reading 'config.json' file")
+	}
+
+	json.Unmarshal(jsonStream, &config)
+
+	db, error := gorm.Open(config.DataBase.Dialect, config.DataBase.ConnectionData)
+	if error != nil {
+		fmt.Errorf("Failed to connection database")
+	}
+
+	db.DB().SetMaxIdleConns(config.DataBase.IdleConns)
+	db.DB().SetMaxOpenConns(config.DataBase.MaxOpenConns)
+
+	//db.LogMode(true)
+	db.AutoMigrate(&User{})
+
+	m := martini.Classic()
+
+	m.Map(db)
+	m.Map(config)
+
+	m.Post("/token", tokenHandler)
+
+	m.Run()
+}
